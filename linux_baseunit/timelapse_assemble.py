96f6fd1136a0c90c2cfdc70e60b0f83ee440be2a
Pragmo <calvineurof@yahoo.com>
2016-11-10 19:42:40 +0000

    tidied user settings and dirs

--- linux_baseunit/timelapse_assemble.py
+++ linux_baseunit/timelapse_assemble.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
 import os
+
+###  Requires MPV to be installed, use - sudo apt install mpv
+
 #user settnigs
 time_jump = 1 #when making timelapse uses every Nth frame so 4 is 4x faster
-outfile = "/timelapse/timelapse.mkv" #directory to save output to default "/timelapse/timelapse.mkv" -this is a relative path starting one directiory above /caps
+darksize = 5000   #all smaller files are removed assumed to be useless 75000 is a good value
+outfps = 10       #10 is a good value, between 2 and 60 is acceptable
+capsdir = "/home/pi/cam_caps/"
+listfile = "/home/pi/ffTL.txt"
+outfile = "/home/pi/timelapse.mkv" #directory to save output to default "/timelapse/timelapse.mkv" -this is a relative path starting one directiory above /caps
 #          /../timelapse/outfile.mkv #would put the files in a directory called timelapse beside the one the script was run from
 #   to use absolute directories to position the outfile alter line 41 removing the .. before "+outfile
 #end of user settings
@@ -13,7 +20,7 @@ faster = []
 fcounter = 0
 
 #grab all the files in /caps directory
-for filefound in os.listdir("./caps/"):
+for filefound in os.listdir(capsdir):
     if filefound.endswith("jpg"):
         fcounter = fcounter + 1
         filelist.append(filefound)
@@ -22,21 +29,24 @@ filelist.sort()
 print "Filelist is now:" + str(len(filelist))
 #make list ignoring the small image assuming they're the datk ones
 for x in filelist:
-    statinfo = os.stat("./caps/" + x)
-    if statinfo.st_size >= 75000: #increase or decrese as needed, 75000 works well for most
+    statinfo = os.stat(capsdir + x)
+    if statinfo.st_size >= darksize: #increase or decrese as needed, 75000 works well for most
         no_dark.append(x)
 print("There are - " + str(len(no_dark)) + " entries in no_dark")
 #make list every Nth file
 for x in range(0, len(no_dark), time_jump):
     faster.append(no_dark[x])
 #writes the file-list for mpv video encoder to use
-os.chdir("./caps/")
-ffTL = open("./ffTL.txt", "w")
+os.chdir(capsdir)
+ffTL = open(listfile, "w")
 for x in faster: #can also be no_dark: or to include dark images change to filelist:
     ffTL.write(x + "\n")
 ffTL.close()
 print "we have " + str(len(faster)) + " files in the faster version..."
 #runs the video encouder
 print "making you a timelapse video..."
-os.system("mpv mf://@./ffTL.txt -mf-fps=10 --ovc libx264 --ovcopts=bitrate=1200:threads=2 -o .."+outfile)
-print "there you go; ."+outfile+" ready to roll.."
+os.system("mpv mf://@"+listfile+" -mf-fps="+str(outfps)+" --ovc libx264 --ovcopts=bitrate=1200:threads=2 -o "+outfile)
+if os.path.isfile(outfile) == True:
+    print "there you go; ."+outfile+" ready to roll.."
+else:
+    print "for some reason the file wasn't created, sorry..."
