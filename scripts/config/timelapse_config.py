38a1230cb6a4370a9921daec6166af23ce1ddb98
Pragmo <calvineurof@yahoo.com>
2016-11-10 12:51:47 +0000

    adds,modified,removes camcap jobs in cron

--- scripts/config/timelapse_config.py
+++ scripts/config/timelapse_config.py
@@ -3,34 +3,61 @@ from crontab import CronTab   #  pip install python-crontab
 
 #script_path = '/home/pragmo/pigitgrow/Pigrow/scripts/cron/'
 script_path = '/home/pi/Pigrow/scripts/cron/'
+cappath = "/home/pi/cam_caps/"
 
 cron = CronTab('root')  #generally leave user as 'root' but 'pi' or whatever will work also
+print("  ############################################")
+print("  ##                                        ##")
+print("  ##     Pigrow2 Cron Camera Management     ##")
+print("  ##                                        ##")
 
-print("  ##                                                             ##")
-print("  ##             Pigrow2 Cron Camera Management                  ##")
-print("  ##                                                             ##")
-
-# Checking current Cron for camera jobs
-
-print("  ##  Checking currently running camcap jobs in cron...")
+print("  ##  Checking cron...")
 camcap_list = []
 for job in cron:
     if str(job).find('camcap') >= 0:
         camcap_list.append(job)
-print("Found " + str(len(camcap_list))+" camcap scripts running in cron.")
+if len(camcap_list) >= 1:
+    print("         -Found " + str(len(camcap_list))+" camcap scripts running...")
+else:
+    print('         -No currently running camcap jobs in cron')
 
 def show_camcap():
+    print("")
+    print("")
+    print("All camcap sctipt jobs currently running on cron;")
     for j in camcap_list:
         print("  - "+ str(j))
+    print("")
 
 def clear_camcap():
-    for j in camcap_list:
-        cron.remove(j)
-        cron.write()
-        print("Job Removed  -  restart program and write the code to refresh the list soon plz")
+    print("")
+    if len(camcap_list) >= 1:
+        print("")
+        print(" Select camcap cron job to remove;")
+        for j in range(0, len(camcap_list)):
+            print("  "+str(j)+" - "+ str(camcap_list[j]))
+        option = raw_input("select job to delete or type ALL to clear all camcap jobs;")
+        if option == ("ALL"):
+            for j in camcap_list:
+                cron.remove(j)
+                cron.write()
+                print("Jobs Cleared...")
+        else:
+            try:
+                cron.remove(camcap_list[int(option)])
+                cron.write()
+                print("Job Removed...")
+            except:
+                print("didn't understand your input, try again;")
+                clear_camcap()
+    else:
+        print(" No camcap jobs to remove")
+        print("")
+        show_cron_menu()
 
 def add_job():
-
+    print("")
+    print("")
     print(" Select camcap script to enable;")
     print("  Webcam;")
     print("    1 - camcap.py                   Basic image only webacm capture script")
@@ -40,6 +67,7 @@ def add_job():
     print("    4 - picamcap.py                 Basic image only capture script")
     print(" soon   5 - picamcap_text_simple.py     Captures picam image and adds text")
     print(" soon   6 - picamcap_text_colour.py     Same as above with colour coding on temp and humid")
+    print("")
     option = raw_input("Type the number and press return;")
     if option == "1":
         cam_script = "python "+script_path+"camcap.py"
@@ -67,28 +95,46 @@ def add_job():
         print "cron error, something is wrong!"
 
 def job_mod():
-    print(" Select camcap cron job to modify;")
-    for j in range(0, len(camcap_list)):
-        print("  "+str(j)+" - "+ str(camcap_list[j]))
-    option = raw_input("select job to modify;")
-    cron.remove(camcap_list[int(option)])
-    cron.write()
-    add_job()
+    print("")
+    if len(camcap_list) >= 1:
+        print("")
+        print(" Select camcap cron job to modify;")
+        for j in range(0, len(camcap_list)):
+            print("  "+str(j)+" - "+ str(camcap_list[j]))
+        option = raw_input("select job to modify;")
+        try:
+            cron.remove(camcap_list[int(option)])
+            cron.write()
+            add_job()
+        except:
+            print("")
+            print("  - Input Error, try again;")
+            job_mod()
+    else:
+        print("No camcap jobs to modify")
+        print("")
+        show_cron_menu()
 
 
 def show_all_cron():
+    print("")
     print(" ## full cron list ## ")
-    for job in cron:
-        print job
+    if len(cron) >= 1:
+        for job in cron:
+            print job
+    else:
+        print(" NO CRON JOBS RUNNING")
+    print(" ####################")
+    print("")
 
 def show_cron_menu():
     print("  ####                                     ####")
     print("  ##                                         ##")
     print("  ##   1, show camcap jobs                   ##")
     print("  ##                                         ##")
-    print("  ##   2, modify camcap job                  ##")
+    print("  ##   2, Modify camcap job                  ##")
     print("  ##                                         ##")
-    print("  ##   3, clear all camcap jobs              ##")
+    print("  ##   3, Delete camcap job                  ##")
     print("  ##                                         ##")
     print("  ##   4, add new camcap job                 ##")
     print("  ##                                         ##")
@@ -96,10 +142,8 @@ def show_cron_menu():
     print("  ##                                         ##")
     print("  ##   6, show all cron jobs                 ##")
     print("  ##                                         ##")
-    print("  ##                                         ##")
     option = raw_input("Type the number and press return;")
     if option == "1":
-        print(" SO YA WANT TO SEE THE CAMCAP JOBS DO YA LADDIE?! WELL HERE YOU GO THEN!")
         show_camcap()
         show_cron_menu()
     elif option == "2":
@@ -110,10 +154,11 @@ def show_cron_menu():
         #show_cron_menu()
     elif option == "4":
         add_job()
-        show_cron_menu()
+        #show_cron_menu()
     elif option == "5":
         print("NOT IMPLIMENTS! NOT IMPLEMENTS! YOU GO NOW!")
 #        ()
+#cappath = "/home/pi/cam_caps/"
 #        show_cron_menu()
     elif option == "6":
         show_all_cron()
--- scripts/cron/camcap.py
+++ scripts/cron/camcap.py
@@ -9,24 +9,25 @@ b_val = "60"
 x_dim = 1600
 y_dim = 1200
 additonal_commands = "-d/dev/video0 -w"
+cappath = "/home/pi/cam_caps/"
 loc_settings = "/home/pi/Pigrow/config/camera_settings.txt"
 try:
     with open(loc_settings, "r") as f:
         for line in f:
             s_item = line.split("=")
-            if s_item[0] == "s_val": 
+            if s_item[0] == "s_val":
                 s_val = s_item[1].split("\n")[0]
-            elif s_item[0] == "c_val": 
+            elif s_item[0] == "c_val":
                 c_val = s_item[1].split("\n")[0]
-            elif s_item[0] == "g_val": 
+            elif s_item[0] == "g_val":
                 g_val = s_item[1].split("\n")[0]
-            elif s_item[0] == "b_val": 
+            elif s_item[0] == "b_val":
                 b_val = s_item[1].split("\n")[0]
-            elif s_item[0] == "x_dim": 
+            elif s_item[0] == "x_dim":
                 x_dim = s_item[1].split("\n")[0]
-            elif s_item[0] == "y_dim": 
+            elif s_item[0] == "y_dim":
                 y_dim = s_item[1].split("\n")[0]
-            elif s_item[0] == "additonal_commands": 
+            elif s_item[0] == "additonal_commands":
                 additonal_commands = s_item[1].split("\n")[0]
 except:
     print("No config file for camera, using default")
@@ -36,5 +37,5 @@ except:
 timenow = time.time()
 timenow = str(timenow)[0:10]
 filename= "cap_"+str(timenow)+".jpg"
-os.system("sudo uvccapture "+additonal_commands+" -S"+s_val+" -C" + c_val + " -G"+ g_val +" -B"+ b_val +" -x"+str(x_dim)+" -y"+str(y_dim)+" -v -t0 -o/home/pi/cam_caps/" + filename)
-print("Image taken and saved to /home/pi/cam_caps/"+filename) 
+os.system("sudo uvccapture "+additonal_commands+" -S"+s_val+" -C" + c_val + " -G"+ g_val +" -B"+ b_val +" -x"+str(x_dim)+" -y"+str(y_dim)+" -v -t0 -o"+cappath+filename)
+print("Image taken and saved to "+cappath+filename)
