b60a819bbc584d17d49f92754f827a3883942fa5
Pragmo <calvineurof@yahoo.com>
2016-11-20 19:01:16 +0000

    added command line arguments

--- linux_baseunit/wallpaper_update.sh
+++ linux_baseunit/wallpaper_update.sh
@@ -3,6 +3,11 @@
 local_dir=/home/pragmo/camcaps/
 #local_dir=/home/pragmo/pigitgrow/Pigrow/graphs/
 
+if [ -z $1 ]
+    then
+      echo File not spesified, using most recent camcaps
+
+
 #lists the most recent file after download
 echo ----------
 echo most recent...
@@ -16,4 +21,11 @@ PID=$(pgrep gnome-session)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 # set's the background in gnome, unity and derivitives
 gsettings set org.gnome.desktop.background picture-uri file://$local_dir$lastfile
+
+
+fi
+
+
+
+gsettings set org.gnome.desktop.background picture-uri file://$1
 echo updated wallpaper
