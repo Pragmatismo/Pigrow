3a4da6ebe659a1377430b84eddc522b32b19da6e
Pragmo <calvineurof@yahoo.com>
2016-12-13 19:35:59 +0000

    modulating like kool keith in a zangy space sub-dimension'

--- scripts/switches/heater_on.py
+++ scripts/switches/heater_on.py
@@ -1,72 +1,50 @@
 #!/usr/bin/python
-import datetime
+import datetime, sys
+sys.path.append('/home/pi/Pigrow/scripts/')
+import pigrow_defs
+
+
 
 print("")
 print("      #############################################")
 print("      ##         Turning the heater - ON         ##")
 
-### user settings
 
-loc_settings = "/home/pragmo/pigitgrow/Pigrow/config/pigrow_config.txt"
-locswitchlog = '/home/pragmo/pigitgrow/Pigrow/logs/switch_log.txt'
 
-### defining variables
 
-pi_set = {}   #the dictionary of settings
+def heater_on(set_dic, switch_log):
+    if 'gpio_heater' in set_dic and not set_dic['gpio_heater'] == '':
+        gpio_pin = int(set_dic['gpio_heater'])
+        gpio_pin_on = set_dic['gpio_heater_on']
+        import RPi.GPIO as GPIO
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(gpio_pin, GPIO.OUT)
+        if gpio_pin_on == "low":
+            GPIO.output(gpio_pin, GPIO.LOW)
+        elif gpio_pin_on == "high":
+            GPIO.output(gpio_pin, GPIO.HIGH)
+        else:
+            print("      !!       CAN'T DETERMINE GPIO DIRECTION    !!")
+            print("      !!  run config program or edit config.txt  !!")
+            print("      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            pigrow_defs.write_log('heater_on.py', 'Failed - no direction set in config', switch_log)
+            exit()
 
-### loading settings
-
-try:
-    with open(loc_settings, "r") as f:
-        for line in f:
-            s_item = line.split("=")
-            pi_set[s_item[0]]=s_item[1].rstrip('\n')
-except:
-    print("Settings not loaded, try running pi_setup")
-    with open(locswitchlog, "a") as f:
-        line = 'heater_on.py @' + str(datetime.datetime.now()) + '@ Failed - settings file error\n'
-        f.write(line)
-    print("Log writen:" + line)
-    raise
-
-# Using settings to do whatever it's supposed to do with them...
-
-if 'gpio_heater' in pi_set and not pi_set['gpio_heater'] == '':
-    gpio_pin = int(pi_set['gpio_heater'])
-    gpio_pin_on = pi_set['gpio_heater_on']
-    import RPi.GPIO as GPIO
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(gpio_pin, GPIO.OUT)
-    #print("skipping gpio module as TESTING TEST TEST TEST")
-    if gpio_pin_on == "low":
-        GPIO.output(gpio_pin, GPIO.LOW)
-        #print("skipping setting gpio LOW as TESTING TEST TEST TEST")
-    elif gpio_pin_on == "high":
-        GPIO.output(gpio_pin, GPIO.HIGH)
-        #print("skipping settubg gpio HIGH as TESTING TEST TEST TEST")
     else:
-        print("      !!       CAN'T DETERMINE GPIO DIRECTION    !!")
+        print("      !!               NO HEATER SET             !!")
         print("      !!  run config program or edit config.txt  !!")
         print("      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        with open(locswitchlog, "a") as f:
-            line = 'heater_on.py @' + str(datetime.datetime.now()) + '@ Failed - no direction set in config\n'
-            f.write(line)
-        print("Log writen:" + line)
+        pigrow_defs.write_log('heater_on.py', 'Failed - due to none set in config', switch_log)
         exit()
 
-else:
-    print("      !!               NO HEATER SET               !!")
-    print("      !!  run config program or edit config.txt  !!")
-    print("      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    with open(locswitchlog, "a") as f:
-        line = 'heater_on.py @' + str(datetime.datetime.now()) + '@ Failed - due to none set in config\n'
-        f.write(line)
+    print("      ##            by switching GPIO "+str(gpio_pin)+" to "+gpio_pin_on+"  ##")
+    print("      #############################################")
+    pigrow_defs.write_log('heater_on.py', 'Heater turned on', switch_log)
     print("Log writen:" + line)
-    exit()
 
-print("      ##            by switching GPIO "+str(gpio_pin)+" to "+gpio_pin_on+"  ##")
-print("      #############################################")
-with open(locswitchlog, "a") as f:
-        line = 'heater_on.py @' + str(datetime.datetime.now()) + '@ Heater turned on  \n'
-        f.write(line)
-print("Log writen:" + line)
+if __name__ == '__main__':
+
+    ### default settings
+    loc_dic = pigrow_defs.load_locs("/home/pi/Pigrow/config/dirlocs.txt")
+    set_dic = pigrow_defs.load_settings(loc_dic['loc_settings'], err_log=loc_dic['err_log'],)
+    heater_on(set_dic, loc_dic['loc_switchlog'])
