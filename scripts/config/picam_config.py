b755d40e32954ee0a0df6f776132ba6820e54383
Pragmo <calvineurof@yahoo.com>
2017-01-07 19:54:15 +0000

    in progress

--- scripts/config/picam_config.py
+++ scripts/config/picam_config.py
@@ -9,7 +9,7 @@ except:
     exit()
 loc_locs = '/home/pi/Pigrow/config/dirlocs.txt'
 loc_dic = pigrow_defs.load_locs(loc_locs)
-caps_path = "./"
+caps_path = loc_dic["caps_path"]
 camera = PiCamera()
 s_val = "10"
 c_val = "10"
