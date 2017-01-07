97fdda309dfafb1f01061c9bfa9e0c197bd4488f
Pragmo <calvineurof@yahoo.com>
2017-01-07 14:38:09 +0000

    fixed link annoyance

--- scripts/autorun/reddit_settings_ear_2.py
+++ scripts/autorun/reddit_settings_ear_2.py
@@ -245,11 +245,11 @@ def write_set(whereto='wiki'):
     cjob=0
     for job in cron:
         cjob=cjob+1
-        modlink = ('https://www.reddit.com/message/compose/?to='+my_username+'&subject=cronmod:' + str(cjob) + '&message=updated_line_here)')
+        modlink = 'https://www.reddit.com/message/compose/?to='+my_username+'&subject=cronmod:' + str(cjob) + '&message=updated_line_here')
         enabled = job.is_enabled()
         page_text += str(enabled) + "|" + str(job.slices) + "|"
         page_text += str(job.command) + "|" + str(job.comment) + "|"
-        page_text += "(modify)["+modlink+"]  \n"
+        page_text += "[modify]("+modlink+")  \n"
     page_text += "  \n  \n"
     page_text += ""
 
