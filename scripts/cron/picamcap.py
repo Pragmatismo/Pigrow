6149b0eafd99e3b04fa8f16ed604beb01a84d8b6
Pragmo <calvineurof@yahoo.com>
2017-01-18 20:58:09 +0000

    returns filename

--- scripts/cron/picamcap.py
+++ scripts/cron/picamcap.py
@@ -65,6 +65,7 @@ def take_picam_raspistill(picam_dic, caps_path):
     except:
         extra_commands = ''
     os.system("raspistill -o "+caps_path+filename+" "+extra_commands)
+    return filename 
 
 
 if __name__ == '__main__':
