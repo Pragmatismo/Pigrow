271be977aa3ae47bb6000f7ce1463d0751eeac98
Pragmo <calvineurof@yahoo.com>
2017-01-28 18:15:21 +0000

    installs dht

--- scripts/config/setup.py
+++ scripts/config/setup.py
@@ -183,6 +183,49 @@ def guided_setup():
     print("I've started writing it, you won't get too far tho...")
     print("checking directories")
     make_dirs()
+    print("Checking Dependencies...")
+    print("  - DHT Sensor;")
+    print(" This is for reading the Temp and Humid sensor, it's supplied by")
+    print("    https://en.wikipedia.org/wiki/Adafruit_Industries")
+    print("")
+    print( "   https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging/software-install-updated")
+    print("")
+    try:
+        import Adafruit_DHT
+        print("- Adafruit DHT driver is installed and working")
+        print("- ")
+    except:
+        try:
+            path = "/home/pragmo/pigitgrow/Pigrow/resources/"
+            print("- Doesn't look like the ADafruit DHT driver is installed,")
+            print("- Installing...")
+            os.chdir(path)
+            print("- Downloading Adafruit_Python_DHT from Github")
+            os.system("git clone https://github.com/adafruit/Adafruit_Python_DHT.git")
+            os.chdir( path + "Adafruit_Python_DHT/")
+            print("- Updating your apt list and installing dependencies,")
+            os.system("sudo apt-get update")
+            os.system("sudo apt-get install build-essential python-dev python-openssl")
+            print("- Dependencies installed, running --: sudo python setup.py install :--")
+            os.system("sudo python setup.py install")
+            print("- Done! ")
+            try:
+                import Adafruit_DHT
+                print("Adafruit_DHT Installed and working.")
+            except:
+                print("...but it doens't seem to have worked...")
+                print(" Try running this script again, if not then")
+                print(" follow the manual install instructions above")
+                print(" and then try this script again...")
+                raise
+        except:
+            print("Install failed, use the install instructions linked above to do it manually.")
+            raise
+        print("")
+        print(" Installing dependencies for pigrow code...")
+        print(" ...ok not yet")
+        exit()
+
 
     print("That's as far as we've got with that.... yeah, not that impressive so far... ")
 
