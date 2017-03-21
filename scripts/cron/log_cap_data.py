93781421c6e8e9a58b7a8194e227d7dc71caaeee
Pragmo <calvineurof@yahoo.com>
2017-03-21 17:21:03 +0000

    very simple, just counts rgb of all pixels and graphs them

---
+++ scripts/cron/log_cap_data.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import os
+import sys
+import numpy
+from PIL import Image
+
+cappath = '/home/pi/Pigrow/caps/'
+logpath = '/home/pi/Pigrow/logs/caps_log.txt'
+
+cappath = '/home/pragmo/frompigrow/GREENHOUSE/caps/'
+logpath = '/home/pragmo/frompigrow/caps_log.txt'
+
+for argu in sys.argv[1:]:
+    try:
+        thearg = str(argu).split('=')[0]
+    except:
+        thearg = str(argu)
+    if thearg == 'cap' or thearg =='caps':
+        cappath = str(argu).split('=')[1]
+    elif thearg == 'l' or thearg == 'log':
+        logpath = str(argu).split('=')[1]
+
+os.chdir(cappath)
+c_photo = max(os.listdir('.'), key = os.path.getctime)
+print c_photo
+
+pil_c_photo = Image.open(c_photo)
+numpy_pic = numpy.array(pil_c_photo)
+r_sum = numpy_pic[:,:,0].sum()
+g_sum = numpy_pic[:,:,1].sum()
+b_sum = numpy_pic[:,:,2].sum()
+tot_sum = r_sum + g_sum + b_sum
+print " ---- Current Photo ---"
+print "Red:" + str(r_sum)
+print "Green:" + str(g_sum)
+print "Blue:" + str(b_sum)
+print "Total;" + str(tot_sum)
+line = str(r_sum) + '>' + str(g_sum) + '>' + str(b_sum) + '>' + str(tot_sum) + '\n'
+with open(logpath, "a") as f:
+    f.write(line)
+print("Log written; " + str(logpath))
