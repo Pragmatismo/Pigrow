220f5b1d75d000883fecfbd2380e4f9f20076348
Pragmo <calvineurof@yahoo.com>
2017-03-26 23:09:18 +0100

    fixed log sillyness

--- scripts/sensors/log_lm393smd.py
+++ scripts/sensors/log_lm393smd.py
@@ -81,7 +81,7 @@ def log_soil_moist_digital(log_path, moist_list):
         moistness = value[0]
         sensor_pin = value[1]
         log_entry += str(moistness) + ":" + str(sensor_pin) + ">"
-    log_entry += log_entry[:-1] + "\n"
+    log_entry = log_entry[:-1] + "\n"
     with open(log_path, "a") as f:
         f.write(log_entry)
     print("Written; " +  log_entry)
