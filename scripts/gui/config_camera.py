88aa6c85679d5112adfb22a0e2cc6f869ba6efcf
Pragmo <calvineurof@yahoo.com>
2017-05-21 16:15:49 +0100

    fswebcam settings added

--- scripts/gui/config_camera.py
+++ scripts/gui/config_camera.py
@@ -78,7 +78,7 @@ def get_cam_config(target_ip, target_user, target_pass, cam_config_loc_on_pi):
             return None
         return localpath
 
-def take_test_image(target_ip, target_user, target_pass, s_val, c_val, g_val, b_val, x_dim=800, y_dim=600, additonal_commands='', cam_capture_choice='uvccapture', output_file='/home/pi/test_cam_settings.jpg'):
+def take_test_image(target_ip, target_user, target_pass, s_val, c_val, g_val, b_val, x_dim=800, y_dim=600, additonal_commands='', cam_capture_choice='uvccapture', output_file='/home/pi/test_cam_settings.jpg', ctrl_test_value=None, ctrl_text_string=None, cmd_str=''):
     found_login = False
     focus_val = "20"
     cam_output = '!!!--NO READING--!!!'
@@ -100,12 +100,16 @@ def take_test_image(target_ip, target_user, target_pass, s_val, c_val, g_val, b_
             cam_cmd  = "fswebcam -r " + str(x_dim) + "x" + str(y_dim)
             cam_cmd += " -D 2"      #the delay in seconds before taking photo
             cam_cmd += " -S 5"      #number of frames to skip before taking image
+            # to list controls use fswebcam -d v4l2:/dev/video0 --list-controls
             cam_cmd += " --set brightness=" + b_val
             cam_cmd += " --set contrast=" + c_val
             cam_cmd += " --set Saturation=" + s_val
             cam_cmd += " --set gain=" + g_val
-            cam_cmd += ' --set "Focus, Auto"=False --set "Focus (absolute)"=' + focus_val
-            #cam_cmd += " --set focus_absolute=10"
+            ##For testing camera ctrl variables
+            if not ctrl_text_string == None:
+                cam_cmd += " --set " + ctrl_text_string + "=" + str(ctrl_test_value)
+            #cam_cmd += ' --set "Focus, Auto"=False --set "Focus (absolute)"=' + focus_val
+            cam_cmd += cmd_str
             cam_cmd += " --jpeg 90" #jpeg quality
             # cam_cmd += " --info HELLO INFO TEXT"
             cam_cmd += " " + output_file  #output filename'
@@ -261,35 +265,56 @@ class config_cam(wx.Frame):
      ## image holder
         self.main_image = wx.StaticBitmap(self, bitmap=wx.EmptyBitmap(1000, 1000), pos=(280, 10))
      ## setup details.
-        wx.StaticText(self,  label='Cam options', pos=(50, 200))
-        wx.StaticText(self,  label='Brightness;', pos=(10, 230))
-        self.tb_b = wx.TextCtrl(self, pos=(120, 230), size=(75, 25))
-        wx.StaticText(self,  label='Contrast;', pos=(10, 260))
-        self.tb_c = wx.TextCtrl(self, pos=(120, 260), size=(75, 25))
-        wx.StaticText(self,  label='Saturation;', pos=(10, 290))
-        self.tb_s = wx.TextCtrl(self, pos=(120, 290), size=(75, 25))
-        wx.StaticText(self,  label='Gain;', pos=(10, 320))
-        self.tb_g = wx.TextCtrl(self, pos=(120, 320), size=(75, 25))
-        wx.StaticText(self,  label='X;', pos=(10, 350))
-        self.tb_x = wx.TextCtrl(self, pos=(120, 350), size=(75, 25))
-        wx.StaticText(self,  label='Y;', pos=(10, 380))
-        self.tb_y = wx.TextCtrl(self, pos=(120, 380), size=(75, 25))
-        wx.StaticText(self,  label='Extra args;', pos=(10, 410))
-        self.tb_extra = wx.TextCtrl(self, pos=(10, 440), size=(200, 25))
-        wx.StaticText(self,  label='capture with', pos=(15, 475))
+        wx.StaticText(self,  label='Cam options', pos=(50, 190))
+        wx.StaticText(self,  label='Brightness;', pos=(10, 220))
+        self.tb_b = wx.TextCtrl(self, pos=(120, 220), size=(75, 25))
+        wx.StaticText(self,  label='Contrast;', pos=(10, 250))
+        self.tb_c = wx.TextCtrl(self, pos=(120, 250), size=(75, 25))
+        wx.StaticText(self,  label='Saturation;', pos=(10, 280))
+        self.tb_s = wx.TextCtrl(self, pos=(120, 280), size=(75, 25))
+        wx.StaticText(self,  label='Gain;', pos=(10, 310))
+        self.tb_g = wx.TextCtrl(self, pos=(120, 310), size=(75, 25))
+        wx.StaticText(self,  label='X;', pos=(10, 340))
+        self.tb_x = wx.TextCtrl(self, pos=(120, 340), size=(75, 25))
+        wx.StaticText(self,  label='Y;', pos=(10, 370))
+        self.tb_y = wx.TextCtrl(self, pos=(120, 370), size=(75, 25))
+     ## camera select
         cam_opts = ['uvccapture', 'fswebcam', 'picam-python', 'picam-cmdline']
-        self.cam_combo = wx.ComboBox(self, choices = cam_opts, pos=(125,470), size=(125, 30))
+        wx.StaticText(self,  label='capture with', pos=(15, 400))
+        self.cam_combo = wx.ComboBox(self, choices = cam_opts, pos=(125,400), size=(125, 30))
         self.cam_combo.Bind(wx.EVT_COMBOBOX, self.cam_combo_go)
+     ## fswebcam only controlls
+        self.fs_label = wx.StaticText(self,  label='fswebcam setting to test', pos=(10, 435))
+        self.list_fs_ctrls_btn = wx.Button(self, label='Show webcam controlls', pos=(25, 460))
+        self.list_fs_ctrls_btn.Bind(wx.EVT_BUTTON, self.list_fs_ctrls_click)
+
+        self.setting_string_label = wx.StaticText(self,  label='set;', pos=(10, 495))
+        self.setting_string_tb = wx.TextCtrl(self, pos=(50, 495), size=(200, 25))
+        self.setting_value_label = wx.StaticText(self,  label='value;', pos=(10, 525))
+        self.setting_value_tb = wx.TextCtrl(self, pos=(60, 525), size=(100, 25))
+        self.add_to_cmd_btn = wx.Button(self, label='Add to cmd string', pos=(5, 550))
+        #self.list_fs_ctrls_btn = wx.Button(self, label='i dunno', pos=(175, 550))
+        self.cmds_string_tb = wx.TextCtrl(self, pos=(10, 590), size=(200, 25))
+        self.add_to_cmd_btn.Bind(wx.EVT_BUTTON, self.add_to_cmd_click)
+        self.fs_label.Hide()
+
+
+
+     ## uvc only contols
+        #wx.StaticText(self,  label='Extra args;', pos=(10, 500))
+        #self.tb_extra = wx.TextCtrl(self, pos=(10, 530), size=(200, 25))
+
+
 
      ## capture image buttons
 
-        self.cap_cam_btn = wx.Button(self, label='Take useing settings', pos=(25, 500))
-        self.cap_unset_btn = wx.Button(self, label='Take using no settings', pos=(25, 540))
-        self.stability_batch_btn = wx.Button(self, label='take test batch', pos=(10, 580))
-        self.graph_batch_btn = wx.Button(self, label='graph batch', pos=(150, 580))
-        self.range_btn = wx.Button(self, label='take a range', pos=(25, 620))
+        self.cap_cam_btn = wx.Button(self, label='Take useing settings', pos=(25, 700))
+        self.cap_unset_btn = wx.Button(self, label='Take using no settings', pos=(25, 740))
+        self.stability_batch_btn = wx.Button(self, label='take test batch', pos=(10, 780))
+        self.graph_batch_btn = wx.Button(self, label='graph batch', pos=(150, 780))
+        self.range_btn = wx.Button(self, label='take a range', pos=(25,820))
         range_opts = ['brightness', 'contrast', 'saturation', 'gain']
-        self.range_combo = wx.ComboBox(self, choices = range_opts, pos=(140,620), size=(125, 30))
+        self.range_combo = wx.ComboBox(self, choices = range_opts, pos=(140,820), size=(125, 30))
         self.cap_cam_btn.Bind(wx.EVT_BUTTON, self.capture_cam_image)
         self.cap_unset_btn.Bind(wx.EVT_BUTTON, self.capture_unset_cam_image)
         self.stability_batch_btn.Bind(wx.EVT_BUTTON, self.stability_batch_btn_click)
@@ -298,13 +323,60 @@ class config_cam(wx.Frame):
 
 
      ## Important wx stuff
-        self.SetSize((900, 800))
+        self.SetSize((900, 900))
         self.SetTitle('Pigrow Control - Camera Config')
         self.Centre()
         self.Show(True)
 
+    def add_to_cmd_click(self, e):
+        test_str = self.setting_string_tb.GetValue()
+        test_val = self.setting_value_tb.GetValue()
+        cmd_str = self.cmds_string_tb.GetValue()
+        cmd_str += ' --set "' + str(test_str) + '"=' + str(test_val)
+        self.cmds_string_tb.SetValue(cmd_str)
+
+    def list_fs_ctrls_click(self, e):
+        print("this is supposed to fswebcam -d v4l2:/dev/video0 --list-controls on the pi")
+        target_ip = self.tb_ip.GetValue()
+        target_user = self.tb_user.GetValue()
+        target_pass = self.tb_pass.GetValue()
+        try:
+            ssh.connect(target_ip, username=target_user, password=target_pass, timeout=3)
+            print "Connected to " + target_ip
+            found_login = True
+            cam_cmd = "fswebcam -d v4l2:/dev/video0 --list-controls"
+            print("---Doing: " + cam_cmd)
+            stdin, stdout, stderr = ssh.exec_command(cam_cmd)
+            cam_output = stderr.read().strip()
+            print "Camera output; " + cam_output
+            ssh.close()
+        except Exception as e:
+            print("Some form of problem; " + str(e))
+            ssh.close()
+        if not cam_output == None:
+            msg_text = 'Camera located and interorgated; copy-paste a controll name from the following into the settings text box \n \n'
+            msg_text += str(cam_output)
+            wx.MessageBox(msg_text, 'Info', wx.OK | wx.ICON_INFORMATION)
+
+
+
     def cam_combo_go(self, e):
-        print("combo box changed, which is nice")
+        if self.cam_combo.GetValue() == 'fswebcam':
+            self.fs_label.Show()
+            self.list_fs_ctrls_btn.Show()
+            self.list_fs_ctrls_btn.Show()
+            self.setting_string_label.Show()
+            self.setting_string_tb.Show()
+            self.setting_value_label.Show()
+            self.setting_value_tb.Show()
+        else:
+            self.fs_label.Hide()
+            self.list_fs_ctrls_btn.Hide()
+            self.list_fs_ctrls_btn.Hide()
+            self.setting_string_label.Hide()
+            self.setting_string_tb.Hide()
+            self.setting_value_label.Hide()
+            self.setting_value_tb.Hide()
 
     def link_with_pi_btn_click(self, e):
         target_ip = self.tb_ip.GetValue()
@@ -340,7 +412,7 @@ class config_cam(wx.Frame):
             self.tb_b.SetValue(str(b_val))
             self.tb_x.SetValue(str(x_dim))
             self.tb_y.SetValue(str(y_dim))
-            self.tb_extra.SetValue(str(additonal_commands))
+            #self.tb_extra.SetValue(str(additonal_commands))
         else:
             print ("Failed to connect")
 
@@ -352,9 +424,22 @@ class config_cam(wx.Frame):
         b_val = str(self.tb_b.GetValue())
         x_dim = str(self.tb_x.GetValue())
         y_dim = str(self.tb_y.GetValue())
-        extra_args = str(self.tb_extra.GetValue())
+        extra_args = '' # str(self.tb_extra.GetValue())
         print s_val, c_val, g_val, b_val, x_dim, y_dim, extra_args, cam_capture_choice
-        found_login, cam_output, output_file = take_test_image(target_ip, target_user, target_pass, s_val, c_val, g_val, b_val, x_dim, y_dim, extra_args, cam_capture_choice)
+        if cam_capture_choice == 'fswebcam':
+            ctrl_text_string = self.setting_string_tb.GetValue()
+            ctrl_test_value = self.setting_value_tb.GetValue()
+            cmd_str = self.cmds_string_tb.GetValue()
+            print("+++++++++++++++++++++++++++=")
+            print(ctrl_test_value)
+            print(ctrl_text_string)
+            print('===========')
+            if not ctrl_test_value == '':
+                found_login, cam_output, output_file = take_test_image(target_ip, target_user, target_pass, s_val, c_val, g_val, b_val, x_dim, y_dim, extra_args, cam_capture_choice, ctrl_test_value=ctrl_test_value, ctrl_text_string=ctrl_text_string, cmd_str=cmd_str)
+            else:
+                found_login, cam_output, output_file = take_test_image(target_ip, target_user, target_pass, s_val, c_val, g_val, b_val, x_dim, y_dim, extra_args, cam_capture_choice, cmd_str=cmd_str)
+        else:
+            found_login, cam_output, output_file = take_test_image(target_ip, target_user, target_pass, s_val, c_val, g_val, b_val, x_dim, y_dim, extra_args, cam_capture_choice)
         photo_location = get_test_pic(target_ip, target_user, target_pass, output_file)
         self.main_image.SetBitmap(wx.BitmapFromImage(wx.Image(photo_location, wx.BITMAP_TYPE_ANY)))
 
