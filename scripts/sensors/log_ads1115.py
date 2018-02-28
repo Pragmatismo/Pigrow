2a3a2430c3becbc35d2715e1938830fa76e712ab
Pragmo <calvineurof@yahoo.com>
2018-02-28 16:40:57 +0000

    converts reading to volts referencing gain

--- scripts/sensors/log_ads1115.py
+++ scripts/sensors/log_ads1115.py
@@ -14,6 +14,7 @@ samples_per_second = 8
 pin_address = "gnd"  #gnd,vdd,sda,sdl - use lower case because argu gets lowercased
 i2c_busnum = 1
 sensor_type = "ads1115"
+convert_volt = True #set false to record results without conversion
 
 homedir = os.environ['HOME']
 
@@ -57,6 +58,10 @@ for argu in sys.argv[1:]:
         print("     at low values the average voltage is taken over")
         print("     a longer period which can be useful in some")
         print("     situations, for faster readings use a higher value.")
+        print("")
+        print(" convert_volt=True/False")
+        print("             When true it converts the recorded values to volts ")
+        print("             referencing the gain value. ")
         sys.exit()
     elif argu == '-flags':
         print("log=[LOG_PATH]")
@@ -65,10 +70,11 @@ for argu in sys.argv[1:]:
         print("sps=[8,16,32,64,128,250,475,860,/ads1015,128,250,490,920,1600,2400,3300]")
         print("pin_address=[0,1]")
         print("i2c_busnum=[gnd,vdd,sda,scl]")
+        print("convert_volt=[True,False]")
         sys.exit()
     elif "=" in argu:
-        thearg = argu.split('=')[0]
-        thevalue = argu.split('=')[1]
+        thearg = argu.split('=')[0].lower()
+        thevalue = argu.split('=')[1].lower()
         if thearg == 'log' or thearg == 'log_path':
             log_path = thevalue
         elif thearg == 'gain':
@@ -81,6 +87,13 @@ for argu in sys.argv[1:]:
             sensor_type = thevalue
         elif thearg == 'samples_per_second' or thearg == 'sps':
             samples_per_second = int(thevalue)
+        elif thearg == "convert_volt" or thearg == "cv" or thearg == "convert_to_volt":
+            if thevalue == "false" or thevalue == 'no':
+                convert_volt = False
+            elif thevalue == "true" or thevalue == 'yes':
+                convert_volt = True
+            else:
+                print("Didn't understand if you wanted to convert to volts or not, use flag convert_volt=false if not")
 
 #setting up the adafruit sensor drivers
 adc = Adafruit_ADS1x15.ADS1115()
@@ -125,6 +138,35 @@ def read_adc():
         print("Reading sensor failed - " + str(e))
         return None
 
+def convert_to_volt(vals):
+    # 2/3 gain =  0.1875mV (default) ###(not actually default from what i can tell)
+    if GAIN == 1:
+        vals[0] = vals[0] * 0.125
+        vals[1] = vals[1] * 0.125
+        vals[2] = vals[2] * 0.125
+        vals[3] = vals[3] * 0.125
+    if GAIN == 2:
+        vals[0] = vals[0] * 0.062
+        vals[1] = vals[1] * 0.062
+        vals[2] = vals[2] * 0.062
+        vals[3] = vals[3] * 0.062
+    if GAIN == 4:
+        vals[0] = vals[0] * 0.03125
+        vals[1] = vals[1] * 0.03125
+        vals[2] = vals[2] * 0.03125
+        vals[3] = vals[3] * 0.03125
+    if GAIN == 8:
+        vals[0] = vals[0] * 0.015625
+        vals[1] = vals[1] * 0.015625
+        vals[2] = vals[2] * 0.015625
+        vals[3] = vals[3] * 0.015625
+    if GAIN == 16:
+        vals[0] = vals[0] * 0.0078125
+        vals[1] = vals[1] * 0.0078125
+        vals[2] = vals[2] * 0.0078125
+        vals[3] = vals[3] * 0.0078125
+    return vals
+
 
 def log_ads1115(log_path, vals):
     timenow = str(datetime.datetime.now())
@@ -141,4 +183,8 @@ def log_ads1115(log_path, vals):
 if __name__ == '__main__':
     vals = read_adc()
     if not vals == None:
+        if not convert_volt == False:
+            convert_to_volt(vals)
         log_ads1115(log_path, vals)
+    else:
+        print("failed to find results")
