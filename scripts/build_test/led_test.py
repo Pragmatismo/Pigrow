f2c2ccc0111aa872f3b3203b765dd2f339184a87
Pragmo <calvineurof@yahoo.com>
2018-03-13 20:33:18 +0000

    added raw input to ask which pin to use

--- scripts/build_test/led_test.py
+++ scripts/build_test/led_test.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
-
+import sys
+import time
 #This will turn the LED on for thirty seconds
 # then turn it off.
+answer = raw_input("Which GPIO is the LED linked to? ")
+try:
+    LED = int(answer)
+except:
+    print("Must be a number")
+    sys.exit()
 
 import RPi.GPIO as GPIO
-import time
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(16,GPIO.OUT)
+GPIO.setup(LED,GPIO.OUT)
 print "LED on"
-GPIO.output(16,GPIO.HIGH)
+GPIO.output(LED,GPIO.HIGH)
 time.sleep(30)
 print "LED off"
-GPIO.output(16,GPIO.LOW)
+GPIO.output(LED,GPIO.LOW)
