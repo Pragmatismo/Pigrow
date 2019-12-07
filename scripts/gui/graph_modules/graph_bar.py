9a68436a6cb890f1b5f2870c8bc8055055769c0d
GrowingRobot <calvineurof@yahoo.com>
2019-12-07 21:02:07 +0000

    added option controls

--- scripts/gui/graph_modules/graph_bar.py
+++ scripts/gui/graph_modules/graph_bar.py
@@ -15,10 +15,11 @@ def read_graph_options():
     '''
     graph_module_settings_dict = {
              "title_text":"",
+             "show_time_period":"true",
              "color_cycle":"false",
-             "line_style":"-",
-             "marker":"o",
-             "show_grid":"true",
+             "bar_width":"0.0005",
+             "bar_alpha":"0.5",
+             "show_grid":"false",
              "major_ticks":"",
              "minor_ticks":"1"
              }
@@ -29,20 +30,20 @@ def make_graph(list_of_datasets, graph_path, ymax="", ymin="", size_h="", size_v
     matplotlib.use('agg')
     import matplotlib.pyplot as plt
     import matplotlib.ticker as plticker
-    print("Want's to create a line graph using the graph_line.py module...  ")
+    print("Want's to create a bar graph using the graph_bar.py module...  ")
 
     # settings
     # load defaults if no settings supplied
     if extra == {}:
         extra = read_graph_options()
     # set variables to settings from dictionary converting to the appropriate type
-    title_text   = extra['title_text']
-    color_cycle  = extra['color_cycle'].lower()
+    title_text       = extra['title_text']
+    show_time_period = extra['show_time_period'].lower()
+    color_cycle = extra['color_cycle'].lower()
     if ',' in color_cycle:
         color_cycle.split(",")
-    line_style   = extra['line_style']
-    marker       = extra['marker']
-    line_flags   = marker + line_style
+    bar_width    = float(extra['bar_width'])
+    bar_alpha    = float(extra['bar_alpha'])
     show_grid    = extra['show_grid'].lower()
     major_ticks  = extra['major_ticks']
     minor_ticks  = extra['minor_ticks']
@@ -57,24 +58,25 @@ def make_graph(list_of_datasets, graph_path, ymax="", ymin="", size_h="", size_v
         date_list = x[0]
         value_list = x[1]
         key_list = x[2]
-        ax.plot(date_list, value_list, line_flags, label=key_list[0], lw=1)
+        ax.bar(date_list, value_list, label=key_list[0], width=bar_width, linewidth=0, alpha=bar_alpha)
 
     # organise the graphing area
     if not major_ticks == "":
-        loc = plticker.MultipleLocator(base=float(major_ticks)) # this locator puts ticks at regular intervals
+        loc = plticker.MultipleLocator(base=float(major_ticks))
         ax.yaxis.set_major_locator(loc)
     if not minor_ticks == "":
-        loc = plticker.MultipleLocator(base=float(minor_ticks)) # this locator puts ticks at regular intervals
+        loc = plticker.MultipleLocator(base=float(minor_ticks))
         ax.yaxis.set_minor_locator(loc)
-    #
     if show_grid == "true":
         plt.grid(axis='y')
-    # Set y axis min and max range
     if len(list_of_datasets) > 1:
         ax.legend()
     else:
         plt.ylabel(key_list[0])
-    plt.title(title_text + "\nTime Perod; " + str(date_list[0].strftime("%b-%d %H:%M")) + " to " + str(date_list[-1].strftime("%b-%d %H:%M")) + " ")
+    if show_time_period == "true":
+        title_text = title_text + "\nTime Perod; " + str(date_list[0].strftime("%b-%d %H:%M")) + " to " + str(date_list[-1].strftime("%b-%d %H:%M"))
+    plt.title(title_text)
+    # Set y axis min and max range
     if not ymax == "":
         plt.ylim(ymax=int(ymax))
     if not ymin == "":
@@ -82,9 +84,8 @@ def make_graph(list_of_datasets, graph_path, ymax="", ymin="", size_h="", size_v
     # format x axis to date
     ax.xaxis_date()
     fig.autofmt_xdate()
-
     # save the graph
     plt.savefig(graph_path)
     # tidy up after ourselves
     plt.close(fig)
-    print("line graph created and saved to " + graph_path)
+    print("bar graph created and saved to " + graph_path)
