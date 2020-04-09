20c3d40314cbe13a69113855fc1ba4475fea8257
GrowingRobot <calvineurof@yahoo.com>
2020-04-09 17:42:57 +0100

    testing modular sensor system

--- scripts/gui/sensor_modules/sensor_ds18b20.py
+++ scripts/gui/sensor_modules/sensor_ds18b20.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+
+or argu in sys.argv[1:]:
+    if "=" in argu:
+        thearg = str(argu).split('=')[0]
+        thevalue = str(argu).split('=')[1]
+        if thearg == 'location':
+            sensor_location = thevalue
+    elif 'help' in argu or argu == '-h':
+        print(" Modular control for ds18b20 temp sensors")
+        print(" ")
+        print(" location=sensors 1wire location")
+        print("          - find using the command")
+        print("            ls /sys/bus/w1/devices/")
+        print("            it should start with 28-")
+        print("")
+        sys.exit(0)
+    elif argu == "-flags":
+        print("location=")
+        sys.exit(0)
+
 class sensor_config():
     connection_type="1wire"
     connection_address_list=["28-021312cdd1aa"]
@@ -8,7 +28,7 @@ class sensor_config():
 def read_sensor(location="", *args):
     import datetime
     print(location)
-    # set up and read the sensor
+    # read the sensor
     sensor_path = "/sys/bus/w1/devices/" + location + "/w1_slave"
     try:
         with open(sensor_path, "r") as sensor_data:
@@ -17,9 +37,7 @@ def read_sensor(location="", *args):
         temp = float(temp[2:]) / 1000
     except:
         print("--problem reading ds18b20")
-        temp = None
         return None
-
     logtime = datetime.datetime.now()
     return [['time',logtime], ['temp', temp]]
 
@@ -28,8 +46,11 @@ if __name__ == '__main__':
     '''
       The ds18b20
       '''
+    if not sensor_location="":
+        output = read_sensor(location=sensor_location)
+    else:
+        print(" No sensor address supplied, this requries a sensor address.")
 
-    output = read_sensor()
     if output == None:
         print("!! Failed to read !!")
     else:
