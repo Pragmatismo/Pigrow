a51037b6e8249812daf4800020829ab2ef495864
GrowingRobot <calvineurof@yahoo.com>
2021-06-10 23:49:44 +0100

    ds18b20 detect from gui

---
+++ scripts/gui/info_modules/info_w1_therm.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import os
+
+def show_info():
+    temp_sensor_list = ""
+    out =  os.popen("ls /sys/bus/w1/devices").read()
+    w1_bus_folders = out.splitlines()
+    for folder in w1_bus_folders:
+        if folder[0:3] == "28-":
+            temp_sensor_list += folder + "\n"
+    return temp_sensor_list.strip()
+
+if __name__ == '__main__':
+    print(show_info())
