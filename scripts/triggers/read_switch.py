72efb7d660eae5c45eb8164a8b75817b40d410db
GrowingRobot <calvineurof@yahoo.com>
2021-08-07 16:22:43 +0100

    testing override switch

---
+++ scripts/triggers/read_switch.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+import os
+import sys
+import datetime
+
+
+def load_config():
+    # Read the settings file
+    homedir = os.getenv("HOME")
+    sys.path.append(homedir + '/Pigrow/scripts/')
+    try:
+        import pigrow_defs
+        script = 'read_switch.py'
+    except:
+        print("pigrow_defs.py not found, unable to continue.")
+        print("make sure pigrow software is installed correctly")
+        sys.exit()
+    loc_dic = pigrow_defs.load_locs(homedir + '/Pigrow/config/dirlocs.txt')
+    pigrow_settings = pigrow_defs.load_settings(loc_dic['loc_settings'])
+    return pigrow_settings
+
+def read_button_settings(pigrow_settings, button_name):
+    # Read the sensor info from the settigns file
+    button_type = None
+    button_log = None
+    button_loc = None
+    #
+    button_key = "button_" + button_name
+    type_key = button_key + "_type"
+    log_key = button_key + "_log"
+    loc_key = button_key + "_loc"
+    if type_key in pigrow_settings:
+        button_type = pigrow_settings[type_key]
+    if log_key in pigrow_settings:
+        button_log = pigrow_settings[log_key]
+    if loc_key in pigrow_settings:
+        button_loc = pigrow_settings[loc_key]
+
+    if button_loc == None:
+        err_msg = button_name + " location not found in settings file."
+        print(err_msg)
+    #    pigrow_defs.write_log('log_sensor_module.py', err_msg, loc_dic['err_log'])
+    #    sys.exit()
+    return button_name, button_type, button_loc, button_log
+
+def read_switch_pos(name, type, loc, log):
+    print("Reading switch " + name)
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(int(loc),GPIO.IN)
+    input = GPIO.input(int(loc))
+    return str(input)
+
+
+
+
+if __name__ == '__main__':
+
+    switch_name = None
+    for argu in sys.argv[1:]:
+        if "=" in argu:
+            thearg = str(argu).split('=')[0]
+            thevalue = str(argu).split('=')[1]
+            if thearg == 'name':
+                switch_name = thevalue
+        elif 'help' in argu or argu == '-h':
+            print(" Script returns the state of a switch")
+            print(" ")
+            print(" This requres the switch to be configured in the remote gui.")
+            print(" ")
+            print(" name=switch unique name")
+            print("")
+            sys.exit(0)
+        elif argu == "-flags":
+            print("name=")
+            sys.exit(0)
+
+    if switch_name == None:
+        print("Switch not identified, please include name= in the commandline arguments.")
+        sys.exit()
+
+    pigrow_settings = load_config()
+    name, type, loc, log = read_button_settings(pigrow_settings, switch_name)
+    position = read_switch_pos(name, type, loc, log)
+    print(name + " is currently " + position)
+    if position == "1":
+        print("activate=True")
