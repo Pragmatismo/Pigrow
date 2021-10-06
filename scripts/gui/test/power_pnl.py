b42cc4401e2930c43153cca627523703841fa646
GrowingRobot <calvineurof@yahoo.com>
2021-10-06 03:12:58 +0100

    can now add and edit config entries

---
+++ scripts/gui/test/power_pnl.py
@@ -0,0 +1,420 @@
+import wx
+
+
+class ctrl_pnl(wx.Panel):
+    #
+    #
+    def __init__( self, parent ):
+        self.parent = parent
+        shared_data = parent.shared_data
+        wx.Panel.__init__ ( self, parent, id = wx.ID_ANY, style = wx.TAB_TRAVERSAL )
+        self.SetBackgroundColour((150,230,170))
+        self.l = wx.StaticText(self,  label=' POWER ')
+        self.fill_table_btn = wx.Button(self, label='Fill Table', size=(75, 30))
+        self.fill_table_btn.Bind(wx.EVT_BUTTON, self.fill_tables_click)
+
+        self.add_hbridge_btn = wx.Button(self, label='Add\nH-Bridge\nControl')
+        self.add_hbridge_btn.Bind(wx.EVT_BUTTON, self.add_hbridge_click)
+
+        # main sizer
+        main_sizer =  wx.BoxSizer(wx.VERTICAL)
+        main_sizer.Add(self.l, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.AddStretchSpacer(1)
+        main_sizer.Add(self.fill_table_btn, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.AddStretchSpacer(1)
+        main_sizer.Add(self.add_hbridge_btn, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.AddStretchSpacer(1)
+        self.SetSizer(main_sizer)
+
+    def fill_tables_click(self, e):
+        self.parent.shared_data.read_pigrow_settings_file()
+        i_pnl = self.parent.dict_I_pnl['power_pnl']
+        i_pnl.motor_ctrl_lst.make_table()
+        i_pnl.pca_device_lst.make_table()
+
+    def add_hbridge_click(self, e):
+        i_pnl = self.parent.dict_I_pnl['power_pnl']
+        # load config from file and fill tables so there are no conflicts
+        self.fill_tables_click("e")
+        # set blanks for dialog box
+        i_pnl.motor_ctrl_lst.s_name = ""
+        i_pnl.motor_ctrl_lst.s_gpioA = ""
+        i_pnl.motor_ctrl_lst.s_gpioB = ""
+        i_pnl.motor_ctrl_lst.s_pwm = ""
+        # call dialog box
+        add_button = hbridge_dialog(i_pnl.motor_ctrl_lst, i_pnl.motor_ctrl_lst.parent)
+        add_button.ShowModal()
+
+
+    def connect_to_pigrow(self):
+        '''
+        This is called every time a connection to a pigrow is made
+        '''
+        pass
+
+class info_pnl(wx.Panel):
+    #
+    #
+    def __init__( self, parent ):
+        self.parent = parent
+        shared_data = parent.shared_data
+        wx.Panel.__init__ ( self, parent, id = wx.ID_ANY, style = wx.TAB_TRAVERSAL )
+        #self.SetBackgroundColour((50,50,50))
+        self.title = wx.StaticText(self,  label=' Power Control Devices')
+
+        # motor control
+        self.motor_l = wx.StaticText(self,  label='L298N - H-Bridge Motor Control')
+        self.motor_help_btn = wx.Button(self, label='Guide', size=(75, 30))
+        self.motor_help_btn.Bind(wx.EVT_BUTTON, self.motor_help_click)
+        motor_l_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        motor_l_sizer.Add(self.motor_l, 0, wx.ALL, 3)
+        motor_l_sizer.AddStretchSpacer(1)
+        motor_l_sizer.Add(self.motor_help_btn, 0, wx.ALL, 3)
+
+        self.motor_ctrl_lst = self.motor_control_list(self, 1)
+        self.motor_ctrl_lst.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.motor_ctrl_lst.doubleclick)
+
+        motor_sizer = wx.BoxSizer(wx.VERTICAL)
+        motor_sizer.Add(motor_l_sizer, 0, wx.ALL|wx.EXPAND, 3)
+        motor_sizer.Add(self.motor_ctrl_lst, 0, wx.ALL|wx.EXPAND, 3)
+
+        # PCA9685
+        self.pca_l = wx.StaticText(self,  label='PCA9685 - PWM Control')
+        self.pca_help_btn = wx.Button(self, label='Guide', size=(75, 30))
+        self.pca_help_btn.Bind(wx.EVT_BUTTON, self.pca_help_click)
+        pca_l_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        pca_l_sizer.Add(self.pca_l, 0, wx.ALL, 3)
+        pca_l_sizer.AddStretchSpacer(1)
+        pca_l_sizer.Add(self.pca_help_btn, 0, wx.ALL, 3)
+
+        self.pca_device_lst = self.pca_list(self, 1)
+        self.pca_device_lst.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.pca_device_lst.doubleclick)
+        self.pca_chan_sizer =  wx.BoxSizer(wx.VERTICAL)
+
+        pca_sizer = wx.BoxSizer(wx.VERTICAL)
+        pca_sizer.Add(pca_l_sizer, 0, wx.ALL|wx.EXPAND, 3)
+        pca_sizer.Add(self.pca_device_lst, 0, wx.ALL|wx.EXPAND, 3)
+        pca_sizer.Add(self.pca_chan_sizer, 0, wx.ALL|wx.EXPAND, 3)
+
+
+        # main sizer
+        main_sizer =  wx.BoxSizer(wx.VERTICAL)
+        main_sizer.Add(self.title, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.AddStretchSpacer(1)
+        main_sizer.Add(motor_sizer, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.AddStretchSpacer(1)
+        main_sizer.Add(pca_sizer, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.AddStretchSpacer(1)
+        #main_sizer.Add(buttons_sizer, 0, wx.ALL|wx.EXPAND, 5)
+        self.make_chan_ctrl()
+        self.SetSizer(main_sizer)
+
+    def motor_help_click(self, e):
+        self.parent.shared_data.show_help('motor_help.png')
+
+    def pca_help_click(self, e):
+        self.parent.shared_data.show_help('pca_help.png')
+
+    def make_chan_ctrl(self):
+        selected = self.pca_device_lst.GetNextSelected(-1)
+        name = ""
+        if not selected == -1:
+            name = self.pca_device_lst.GetItemText(selected, 0)
+        self.pca_chan_l = wx.StaticText(self,  label='PCA Channel info ' + name)
+
+        self.pca_chan_sizer.Add(self.pca_chan_l, 0, wx.ALL|wx.EXPAND, 5)
+        self.Layout()
+        self.parent.Layout()
+
+    class motor_control_list(wx.ListCtrl):
+        def __init__(self, parent, id):
+            self.parent = parent
+            wx.ListCtrl.__init__(self, parent, id, style=wx.LC_REPORT)
+            self.InsertColumn(0, 'Unique Name')
+            self.InsertColumn(1, 'gpio A')
+            self.InsertColumn(2, 'gpio B')
+            self.InsertColumn(3, 'pwm control')
+            self.autosizeme()
+
+        def make_table(self):
+            config_dict = self.parent.parent.shared_data.config_dict
+            motor_list = []
+            print("  - Using config_dict to fill motor table")
+            self.DeleteAllItems()
+            # Create a list of items
+            for key, value in list(config_dict.items()):
+                if "hbridge_" in key:
+                    name = key.split("_")[1]
+                    if not name in motor_list:
+                        motor_list.append(name)
+
+            for motor_name in motor_list:
+                gpioA, gpioB, pwm = self.read_motor_conf(motor_name, config_dict, "hbridge_")
+                self.add_to_motor_table(motor_name, gpioA, gpioB, pwm)
+
+        def read_motor_conf(self, item_name, config_dict, prefix):
+            # Extract sensor config info from config dictionary
+            field_list = ["_gpioA",
+                          "_gpioB",
+                          "_pwm"]
+            info = []
+            for field in field_list:
+                field_key = prefix + item_name + field
+                if field_key in config_dict:
+                    info.append(config_dict[field_key])
+                else:
+                    info.append("")
+            return info
+
+        def autosizeme(self):
+            if self.GetItemCount() == 0:
+                self.SetColumnWidth(0, wx.LIST_AUTOSIZE_USEHEADER)
+                self.SetColumnWidth(1, wx.LIST_AUTOSIZE_USEHEADER)
+                self.SetColumnWidth(2, wx.LIST_AUTOSIZE_USEHEADER)
+                self.SetColumnWidth(3, wx.LIST_AUTOSIZE_USEHEADER)
+            else:
+                self.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+                self.SetColumnWidth(1, wx.LIST_AUTOSIZE)
+                self.SetColumnWidth(2, wx.LIST_AUTOSIZE)
+                self.SetColumnWidth(3, wx.LIST_AUTOSIZE)
+
+        def add_to_motor_table(self, name, gpioA, gpioB, pwm_ctrl):
+            self.InsertItem(0, str(name))
+            self.SetItem(0, 1, str(gpioA))
+            self.SetItem(0, 2, str(gpioB))
+            self.SetItem(0, 3, str(pwm_ctrl))
+
+        def doubleclick(self, e):
+            shared_data = self.parent.parent.shared_data
+
+            index =  e.GetIndex()
+            #get info for dialogue box
+            self.s_name  = self.GetItem(index, 0).GetText()
+            self.s_gpioA = self.GetItem(index, 1).GetText()
+            self.s_gpioB = self.GetItem(index, 2).GetText()
+            self.s_pwm   = self.GetItem(index, 3).GetText()
+            h_dialog_box = hbridge_dialog(self, self.parent)
+            h_dialog_box.ShowModal()
+
+
+    class pca_list(wx.ListCtrl):
+        def __init__(self, parent, id):
+            self.parent = parent
+            wx.ListCtrl.__init__(self, parent, id, style=wx.LC_REPORT)
+            self.InsertColumn(0, 'Name')
+            self.InsertColumn(1, 'i2c address')
+            self.InsertColumn(2, 'Frequency')
+            self.autosizeme()
+
+        def make_table(self):
+            print(" pca_list - make_table does nothing, so far")
+            config_dict = self.parent.parent.shared_data.config_dict
+            pca_list = []
+            print("  - Using config_dict to fill pca table")
+            self.DeleteAllItems()
+            # Create a list of items
+            for key, value in list(config_dict.items()):
+                if "pca_" in key:
+                    name = key.split("_")[1]
+                    if not name in pca_list:
+                        pca_list.append(name)
+
+            for pca_name in pca_list:
+                i2c, freq = self.read_pca_conf(pca_name, config_dict, "pca_")
+                self.add_to_pca_list(pca_name, i2c, freq)
+
+        def read_pca_conf(self, item_name, config_dict, prefix):
+            # Extract sensor config info from config dictionary
+            field_list = ["_loc",
+                          "_freq"]
+            info = []
+            for field in field_list:
+                field_key = prefix + item_name + field
+                if field_key in config_dict:
+                    info.append(config_dict[field_key])
+                else:
+                    info.append("")
+            return info
+
+        def autosizeme(self):
+            if self.GetItemCount() == 0:
+                self.SetColumnWidth(0, wx.LIST_AUTOSIZE_USEHEADER)
+                self.SetColumnWidth(1, wx.LIST_AUTOSIZE_USEHEADER)
+                self.SetColumnWidth(2, wx.LIST_AUTOSIZE_USEHEADER)
+            else:
+                self.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+                self.SetColumnWidth(1, wx.LIST_AUTOSIZE)
+                self.SetColumnWidth(2, wx.LIST_AUTOSIZE)
+
+        def add_to_pca_list(self, name, i2c, freq):
+            self.InsertItem(0, str(name))
+            self.SetItem(0, 1, str(i2c))
+            self.SetItem(0, 2, str(freq))
+
+        def doubleclick(self, e):
+            shared_data = self.parent.parent.shared_data
+            index =  e.GetIndex()
+            name = self.GetItem(index, 0).GetText()
+            print(" Sorry double clicking on " + name + " does nothing.")
+
+
+class hbridge_dialog(wx.Dialog):
+    def __init__(self, parent, *args, **kw):
+        self.parent = parent
+        super(hbridge_dialog, self).__init__(*args, **kw)
+        self.InitUI()
+        self.SetSize((800, 700))
+        self.SetTitle("H-Bridge setup")
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+
+    def InitUI(self):
+        i_pnl = self.parent.parent.parent.dict_I_pnl['power_pnl']
+        shared_data = self.parent.parent.parent.shared_data
+        #link_pnl = self.parent.parent.parent.link_pnl
+        self.motor_ctrl_lst = i_pnl.motor_ctrl_lst
+        # read values - blank for adding a new sensor, used when editing existing entry
+        self.s_name  = self.motor_ctrl_lst.s_name
+        self.s_gpioA = self.motor_ctrl_lst.s_gpioA
+        self.s_gpioB = self.motor_ctrl_lst.s_gpioB
+        self.s_pwm   = self.motor_ctrl_lst.s_pwm
+
+        # panel
+        pnl = wx.Panel(self)
+
+        # Header
+        box_label = wx.StaticText(self,  label='H-bridge motor controller')
+        box_label.SetFont(shared_data.title_font)
+        # Show guide button
+        show_guide_btn = wx.Button(self, label='Guide', size=(175, 30))
+        show_guide_btn.Bind(wx.EVT_BUTTON, self.show_guide_click)
+        header_sizer =  wx.BoxSizer(wx.HORIZONTAL)
+        header_sizer.Add(box_label, 0, wx.ALL, 5)
+        header_sizer.AddStretchSpacer(1)
+        header_sizer.Add(show_guide_btn, 0, wx.ALL, 5)
+
+        ## unique name
+        name_label = wx.StaticText(self,  label='Unique name')
+        self.name_tc = wx.TextCtrl(self, value=self.s_name, size=(200,30))
+        name_sizer =  wx.BoxSizer(wx.HORIZONTAL)
+        name_sizer.Add(name_label, 0, wx.ALL|wx.EXPAND, 5)
+        name_sizer.Add(self.name_tc, 0, wx.ALL|wx.EXPAND, 5)
+        ## gpioA
+        gpioA_label = wx.StaticText(self,  label='gpioA')
+        self.gpioA_tc = wx.TextCtrl(self, value=self.s_gpioA, size=(200,30))
+        gpioA_sizer =  wx.BoxSizer(wx.HORIZONTAL)
+        gpioA_sizer.Add(gpioA_label, 0, wx.ALL|wx.EXPAND, 5)
+        gpioA_sizer.Add(self.gpioA_tc, 0, wx.ALL|wx.EXPAND, 5)
+        ## gpioB
+        gpioB_label = wx.StaticText(self,  label='gpioB')
+        self.gpioB_tc = wx.TextCtrl(self, value=self.s_gpioB, size=(200,30))
+        gpioB_sizer =  wx.BoxSizer(wx.HORIZONTAL)
+        gpioB_sizer.Add(gpioB_label, 0, wx.ALL|wx.EXPAND, 5)
+        gpioB_sizer.Add(self.gpioB_tc, 0, wx.ALL|wx.EXPAND, 5)
+
+        # Read motor directon
+        self.read_m_btn = wx.Button(self, label='Read Motor Direction', size=(175, 30))
+        self.read_m_btn.Bind(wx.EVT_BUTTON, self.read_motor_directon)
+        self.read_m_label = wx.StaticText(self,  label='')
+        self.switch_m_d_btn = wx.Button(self, label='Switch Motor Direction', size=(175, 30))
+        self.switch_m_d_btn.Bind(wx.EVT_BUTTON, self.switch_m_d)
+        read_m_sizer =  wx.BoxSizer(wx.HORIZONTAL)
+        read_m_sizer.Add(self.read_m_btn, 0, wx.ALL|wx.EXPAND, 5)
+        read_m_sizer.Add(self.read_m_label, 0, wx.ALL|wx.EXPAND, 5)
+        read_m_sizer.Add(self.switch_m_d_btn, 0, wx.ALL|wx.EXPAND, 5)
+
+        # buttons_
+        self.save_btn = wx.Button(self, label='Save', size=(175, 30))
+        self.save_btn.Bind(wx.EVT_BUTTON, self.save_click)
+        self.cancel_btn = wx.Button(self, label='Cancel', size=(175, 30))
+        self.cancel_btn.Bind(wx.EVT_BUTTON, self.OnClose)
+        buttons_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        buttons_sizer.AddStretchSpacer(1)
+        buttons_sizer.Add(self.save_btn, 0,  wx.ALL, 3)
+        buttons_sizer.AddStretchSpacer(1)
+        buttons_sizer.Add(self.cancel_btn, 0,  wx.ALL, 3)
+        buttons_sizer.AddStretchSpacer(1)
+
+        main_sizer =  wx.BoxSizer(wx.VERTICAL)
+        main_sizer.Add(header_sizer, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.AddStretchSpacer(1)
+        main_sizer.Add(name_sizer, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.Add(gpioA_sizer, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.Add(gpioB_sizer, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.Add(read_m_sizer, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.AddStretchSpacer(1)
+        main_sizer.Add(buttons_sizer, 0, wx.ALL|wx.EXPAND, 5)
+        self.SetSizer(main_sizer)
+
+    def show_guide_click(self, e):
+        self.parent.parent.parent.shared_data.show_help('motor_help.png')
+
+    def OnClose(self, e):
+        self.motor_ctrl_lst.s_name  = ""
+        self.motor_ctrl_lst.s_gpioA = ""
+        self.motor_ctrl_lst.s_gpioB = ""
+        self.motor_ctrl_lst.s_pwm   = ""
+        self.Destroy()
+
+    def save_click(self, e):
+        shared_data = self.parent.parent.parent.shared_data
+        n_name  = self.name_tc.GetValue()
+        n_gpioA = self.gpioA_tc.GetValue()
+        n_gpioB = self.gpioB_tc.GetValue()
+        #n_pwm   =
+        # as yet unused self.s_pwm
+        changed = "yes"
+        if self.s_name == n_name:
+            if self.s_gpioA == n_gpioA:
+                if self.s_gpioB == n_gpioB:
+                    changed = None
+
+        if changed == None:
+            print(" - Nothing changed, no need to save ")
+        else:
+            print(" - Something changed, not saving though that's not coded yet")
+            name_start = "hbridge_" + n_name
+            shared_data.config_dict[name_start + "_gpioA"] = n_gpioA
+            shared_data.config_dict[name_start + "_gpioB"] = n_gpioB
+            #shared_data.config_dict[name_start + "_pwm"] = n_pwm
+
+            # If name changed delete old entries
+            if not n_name == self.s_name:
+                name_start = "hbridge_" + self.s_name
+                possible_keys = [name_start + "_gpioA",
+                                 name_start + "_gpioB",
+                                 name_start + "_pwm"]
+                for possible_key in possible_keys:
+                    if possible_key in shared_data.config_dict:
+                        del shared_data.config_dict[possible_key]
+
+            shared_data.update_pigrow_config_file_on_pi()
+        self.Destroy()
+
+
+    def read_motor_directon(self, e):
+        gpio_a = self.gpioA_tc.GetValue()
+        gpio_b = self.gpioB_tc.GetValue()
+        state_a = self.read_pin_state(gpio_a)
+        state_b = self.read_pin_state(gpio_b)
+        msg = "A = " + state_a + "  B = " + state_b + "\n"
+        if state_a == "1" and state_b == "0":
+            msg += "Direction set to +"
+        if state_b == "0" and state_a == "1":
+            msg += "Direction set to -"
+        else:
+            msg += "Not a valid direction setting."
+        self.read_m_label.SetLabel(msg)
+
+    def read_pin_state(self, gpio_pin):
+        # create info on gpio pin
+        cmd = "echo " + str(gpio_pin) + " > /sys/class/gpio/export"
+        out, error = self.parent.parent.parent.link_pnl.run_on_pi(cmd)
+        # read the pins value
+        cmd = "cat /sys/class/gpio/gpio" + str(gpio_pin) + "/value"
+        out, error = self.parent.parent.parent.link_pnl.run_on_pi(cmd)
+        gpio_status = out.strip()
+        return gpio_status
+
+
+    def switch_m_d(self, e):
+        print(" THIS BUTTON DOES NOTHING lol")
