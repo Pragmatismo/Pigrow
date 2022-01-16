0d4c3532c034157d0d047f014ebe82eaf6ebeac6
GrowingRobot <calvineurof@yahoo.com>
2022-01-16 18:29:20 +0000

    changed mode

--- scripts/gui/sensor_modules/sensor_veml7700.py
+++ scripts/gui/sensor_modules/sensor_veml7700.py
@@ -5,46 +5,38 @@ class sensor_config():
     # find connected sensors
     def find_settings():
         print("connection_type=i2c")
-        print("connection_address_list=0x44,0x45")
-        print("default_connection_address=0x44")
-        
-        
-        
-def read_sensor(location=""):
+        print("connection_address_list=0x10")
+        print("default_connection_address=0x10")
+
+
+
+def read_sensor(location="", extra="", sensor_name="", *args):
     # Try importing the modules then give-up and report to user if it fails
     import datetime
     try:
         import board
-        import busio
-        import adafruit_sht31d
+        import adafruit_veml7700
     except:
-        print("adafruit_sht31d module not installed, install using the command;")
-        print("   sudo pip3 install adafruit-circuitpython-sht31d")
+        print("adafruit_si7021 module not installed, install using the command;")
+        print("     sudo pip3 install adafruit-circuitpython-si7021")
         return None
 
     # set up and read the sensor
     try:
-        i2c = busio.I2C(board.SCL, board.SDA)
-        sensor = adafruit_sht31d.SHT31D(i2c, location)
-        temperature = sensor.temperature
-        humidity = sensor.relative_humidity
-        if humidity == None or temperature == None or humidity > 101:
-            print("--problem reading sht31d")
-            return None
-        else:
-            humidity = round(humidity,2)
-            temperature = round(temperature, 2)
-            logtime = datetime.datetime.now()
-            return [['time',logtime], ['humid', humidity], ['temperature', temperature]]
+        i2c = board.I2C()
+        sensor = adafruit_veml7700.VEML7700(i2c)
+        lux = sensor.light
+        logtime = datetime.datetime.now()
+        return [['time',logtime], ['lux', lux]]
     except:
-        print("--problem reading sht31d")
+        print("--problem reading veml7700 lux sensor;", e)
         return None
 
 
 if __name__ == '__main__':
+    '''
+      Currently only works with veml7700 on the default address of 0x10
       '''
-      Not implemented: The sht31d can be configured to enable the heater 
-      Implemented: and switch addresses between 0x44 and 0x45'''
      # check for command line arguments
     sensor_location = ""
     for argu in sys.argv[1:]:
@@ -54,7 +46,7 @@ if __name__ == '__main__':
             if thearg == 'location':
                 sensor_location = thevalue
         elif 'help' in argu or argu == '-h':
-            print(" Modular control for AdaSHT31D")
+            print(" Modular control for veml7700 lux sensor")
             print(" ")
             print("")
             print(" -config  ")
@@ -67,7 +59,7 @@ if __name__ == '__main__':
             sensor_config.find_settings()
             sys.exit()
     # read sensor
-    output = read_sensor(location=sensor_location)
+    output = read_sensor()
     if output == None:
         print("!! Failed to read !!")
     else:
