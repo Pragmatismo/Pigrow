47817798934dec4ff3c525fb2a70ae2bb9fea84e
GrowingRobot <calvineurof@yahoo.com>
2022-11-18 20:36:09 +0000

    added table for led list

--- scripts/gui/test/display_pnl.py
+++ scripts/gui/test/display_pnl.py
@@ -23,7 +23,9 @@ class ctrl_pnl(wx.Panel):
         print("display pannel connected to pigrow and doing nothing with it :D lol")
 
     def refresh_click(self, e):
-        print("You pressed a button and all it does is show this text")
+        self.parent.shared_data.read_pigrow_settings_file()
+        i_pnl = self.parent.dict_I_pnl['display_pnl']
+        i_pnl.led_lst.make_table()
 
 
 class info_pnl(scrolled.ScrolledPanel):
@@ -47,9 +49,31 @@ class info_pnl(scrolled.ScrolledPanel):
         led_title =  wx.StaticText(self,  label='LED')
         led_guide_btn = wx.Button(self, label='Guide')
         led_guide_btn.Bind(wx.EVT_BUTTON, self.led_guide_click)
+        # list ctrl containing leds
+        self.led_lst = self.led_list(self, 1)
+        self.led_lst.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.led_lst.doubleclick)
+        # LED button bar
+        new_led_btn = wx.Button(self, label='Add LED')
+        new_led_btn.Bind(wx.EVT_BUTTON, self.new_led_click)
+        del_led_btn = wx.Button(self, label='del LED')
+        del_led_btn.Bind(wx.EVT_BUTTON, self.del_led_click)
+        led_but_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        led_but_sizer.Add(new_led_btn, 1,wx.ALL, 5)
+        led_but_sizer.Add(del_led_btn, 1,wx.ALL, 5)
+
+
+        # led dialog box
+        #      - set gpio
+        #      - test led
+        #      - make cmd for copypaste / trigger
+        #             - set mode, if manual slso blink time on and off
+        #             - auto include name of led
+
         led_sizer = wx.BoxSizer(wx.VERTICAL)
-        led_sizer.Add(led_title, 1,wx.ALL, 5)
-        led_sizer.Add(led_guide_btn, 1,wx.ALL|wx.ALIGN_RIGHT, 5)
+        led_sizer.Add(led_title, 0,wx.ALL, 5)
+        led_sizer.Add(led_guide_btn, 0, wx.ALL|wx.ALIGN_RIGHT, 5)
+        led_sizer.Add(self.led_lst, 1,wx.ALL, 5)
+        led_sizer.Add(led_but_sizer, 1,wx.ALL, 5)
 
 
         # Datawall
@@ -77,3 +101,80 @@ class info_pnl(scrolled.ScrolledPanel):
 
     def datawall_guide_click(self, e):
         self.parent.shared_data.show_help('datawall_display_help.png')
+
+    def new_led_click(self, e):
+        print(" Sorry you can't add leds at this time")
+
+    def del_led_click(self, e):
+        print(" Sorry, deleting LEDs isn't coded yet.")
+
+    class led_list(wx.ListCtrl):
+        def __init__(self, parent, id):
+            self.parent = parent
+            wx.ListCtrl.__init__(self, parent, id, size=(800, 150), style=wx.LC_REPORT)
+            self.InsertColumn(0, 'Unique Name')
+            self.InsertColumn(1, 'GPIO pin')
+            self.InsertColumn(2, 'set on reboot')
+            self.InsertColumn(3, 'status')
+            self.autosizeme()
+
+        def make_table(self):
+            config_dict = self.parent.parent.shared_data.config_dict
+            led_list = []
+            print("  - Using config_dict to fill led table")
+            self.DeleteAllItems()
+            # Create a list of items
+            for key, value in list(config_dict.items()):
+                if "led_" in key:
+                    name = key.split("_")[1]
+                    if not name in led_list:
+                        led_list.append(name)
+
+            for name in led_list:
+                loc, reboot = self.read_conf(name, config_dict, "led_")
+                self.add_to_relay_table(name, loc, reboot)
+
+            self.autosizeme()
+
+        def read_conf(self, item_name, config_dict, prefix):
+            # Extracting config info from config dictionary
+            field_list = ["_loc",
+                          "_reboot"]
+            info = []
+            for field in field_list:
+                field_key = prefix + item_name + field
+                if field_key in config_dict:
+                    info.append(config_dict[field_key])
+                else:
+                    info.append("")
+            return info
+
+
+        def autosizeme(self):
+            for i in range(0, self.GetColumnCount()):
+                self.SetColumnWidth(i, wx.LIST_AUTOSIZE)
+                l = self.GetColumnWidth(i)
+                self.SetColumnWidth(i, wx.LIST_AUTOSIZE_USEHEADER)
+                h = self.GetColumnWidth(i)
+                if l > h:
+                    self.SetColumnWidth(i, wx.LIST_AUTOSIZE)
+
+        def add_to_relay_table(self, name, loc, reboot):
+            self.InsertItem(0, str(name))
+            self.SetItem(0, 1, str(loc))
+            self.SetItem(0, 2, str(reboot))
+            status = get_led_status(name, loc)
+            self.SetItem(0, 3, str(status))
+
+        def get_led_status(self):
+            return "not coded"
+
+        def doubleclick(self, e):
+            index =  e.GetIndex()
+            #get info for dialogue box
+            # tank_dialog.s_name  = self.GetItem(index, 0).GetText()
+            # tank_dialog.s_volume = self.GetItem(index, 1).GetText()
+            # tank_dialog.s_mode = self.GetItem(index, 2).GetText()
+            # relay_box = tank_dialog(self.parent.c_pnl, self.parent.c_pnl.parent)
+            # relay_box.ShowModal()
+            # self.parent.c_pnl.fill_table_click("e")
