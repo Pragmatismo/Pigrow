99d567ff3e5cdfe73750a986228cde1f13f143eb
GrowingRobot <calvineurof@yahoo.com>
2022-11-19 23:33:52 +0000

    trying more popen flags

--- scripts/triggers/set_led.py
+++ scripts/triggers/set_led.py
@@ -80,7 +80,7 @@ def set_led_blink(name, mode, gpio):
         print("     set=fast")
         sys.exit()
     cmd = [homedir + "/Pigrow/scripts/persistent/blink_led.py", "name=" + name, " speed=" + speed]
-    subprocess.Popen(cmd, capture_output=False)
+    subprocess.Popen(cmd)
 
 def write_onboot(name, made):
     led_stat_path = homedir + "/Pigrow/logs/ledstat_" + name + ".txt"
