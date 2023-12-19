50ba129f385dad63b048c09343d96c23c6e5ee2c
Calvin <calvineurof@yahoo.com>
2023-12-19 01:39:43 +0000

    added image size limits

--- scripts/gui/test/timelapse_modules/analyse_colourswatch.py
+++ scripts/gui/test/timelapse_modules/analyse_colourswatch.py
@@ -11,19 +11,34 @@ def analyse_set(ani_frame_list, temp_folder):
     first_image = Image.open(ani_frame_list[0])
     width, height = first_image.size
 
+    # Set the maximum image size
+    max_image_size = 10000
+
+    # Check if the resulting image would exceed the maximum size
+    bar_size = 25
+    result_width = len(ani_frame_list) * bar_size
+    while result_width > max_image_size:
+        bar_size = bar_size - 2
+        if bar_size <= 0:
+            print("Frame list too long to create swatch, sorry")
+            return "error - frame list too long"
+        result_width = len(ani_frame_list) * bar_size
+    print("Bar size set to", bar_size)
+
+
     # Create a background image to paste strips onto
-    result_image = Image.new("RGBA", (len(ani_frame_list) * 25, height), (0, 0, 0, 0))
+    result_image = Image.new("RGBA", (result_width, height), (0, 0, 0, 0))
 
     # Iterate through the image list and paste strips onto the result image
     for i, image_path in enumerate(ani_frame_list):
         # Open each image and extract a 25-pixel-wide strip from the center
         current_image = Image.open(image_path)
-        left = (width - 25) // 2
-        right = left + 25
+        left = (width - bar_size) // 2
+        right = left + bar_size
         strip = current_image.crop((left, 0, right, height))
 
         # Paste the strip onto the result image
-        result_image.paste(strip, (i * 25, 0))
+        result_image.paste(strip, (i * bar_size, 0))
 
     # Save or display the result image as needed
     result_image.save(temp_folder + "/result_image.png")
--- scripts/gui/test/timelapse_modules/analyse_filesize.py
+++ scripts/gui/test/timelapse_modules/analyse_filesize.py
@@ -14,10 +14,24 @@ def analyse_set(ani_frame_list, temp_folder):
     # Set the figure size based on the number of data points
     num_data_points = len(file_sizes)
     fig_width = max(1, num_data_points * 0.15)  # Adjust the minimum figure width as needed
+
+    # Set the maximum image size
+    max_image_size = 75000
+
+    # Check if the resulting image would exceed the maximum size
+    result_width = int(fig_width * 100)  # Assuming each bar has a width of 100 pixels
+    result_height = 1000  # Assuming the height of the bar graph is 1000 pixels
+    if result_width > max_image_size or result_height > max_image_size:
+        print("Error: Resulting image exceeds the maximum allowed size.")
+        return
+
     plt.figure(figsize=(fig_width, 10))
 
     # Create a bar graph
     plt.bar(range(len(file_sizes)), file_sizes, color='blue', width=0.6)
+
+    # Adjust the layout to reduce empty space
+    plt.subplots_adjust(left=0.005, right=0.99, top=0.9, bottom=0.1)
 
     plt.savefig(temp_folder + "/file_size_graph.png")
     plt.close()
