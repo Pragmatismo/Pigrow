612b35f97fa4907d742fe77b852d45cc29f438a9
RoboGroMo <calvineurof@yahoo.com>
2025-05-16 01:18:54 +0100

    fixed to only return images

--- scripts/gui/info_modules/picture_recent.py
+++ scripts/gui/info_modules/picture_recent.py
@@ -2,21 +2,25 @@
 import os
 homedir = os.getenv("HOME")
 
-def show_info():
-    # conected network
-    # Read the currently connected network name
-    caps_path = homedir + "/Pigrow/caps/"
-    file_list = []
-    if os.path.isdir(caps_path):
-        out =  os.popen("ls " + caps_path).read()
-        file_list = out.split()
-        file_list.sort()
 
-    if len(file_list) > 0:
-        return caps_path + file_list[-1]
-    else:
+def show_info():
+    caps_path = os.path.join(homedir, "Pigrow", "caps")
+    if not os.path.isdir(caps_path):
         return "none"
 
+    # get a sorted list of all entries in caps_path
+    files = sorted(os.listdir(caps_path))
+
+    # define which extensions count as "images"
+    image_exts = {".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff"}
+
+    # scan from the end; return the first image we find
+    for fname in reversed(files):
+        ext = os.path.splitext(fname)[1].lower()
+        if ext in image_exts:
+            return os.path.join(caps_path, fname)
+
+    return "none"
 
 if __name__ == '__main__':
     print(show_info())
