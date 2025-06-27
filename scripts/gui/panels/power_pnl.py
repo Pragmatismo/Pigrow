78de227447ba96599e764cb57f2da9a11417bfe7
RoboGroMo <calvineurof@yahoo.com>
2025-06-27 17:52:40 +0100

    framework for lamp confirmation tool

--- scripts/gui/panels/power_pnl.py
+++ scripts/gui/panels/power_pnl.py
@@ -13,16 +13,19 @@ class ctrl_pnl(wx.Panel):
         self.fill_table_btn = wx.Button(self, label='Fill Table', size=(75, 30))
         self.fill_table_btn.Bind(wx.EVT_BUTTON, self.fill_tables_click)
 
-        self.add_relay_btn = wx.Button(self, label='Add\nRelay\nControl')
+        self.add_relay_btn = wx.Button(self, label='Add\nRelay Control')
         self.add_relay_btn.Bind(wx.EVT_BUTTON, self.add_relay_click)
 
-        self.add_hbridge_btn = wx.Button(self, label='Add\nH-Bridge\nControl')
+        self.lamp_confirm_btn = wx.Button(self, label='Config\nLamp Confirm')
+        self.lamp_confirm_btn.Bind(wx.EVT_BUTTON, self.lamp_confirm_click)
+
+        self.add_hbridge_btn = wx.Button(self, label='Add\nH-Bridge Control')
         self.add_hbridge_btn.Bind(wx.EVT_BUTTON, self.add_hbridge_click)
 
-        self.add_pca_btn = wx.Button(self, label='Add\npca9685\nControl')
+        self.add_pca_btn = wx.Button(self, label='Add\npca9685 Control')
         self.add_pca_btn.Bind(wx.EVT_BUTTON, self.add_pca_click)
 
-        self.add_hwpwm_btn = wx.Button(self, label='Add\nHardware PWM\n')
+        self.add_hwpwm_btn = wx.Button(self, label='Add\nHardware PWM')
         self.add_hwpwm_btn.Bind(wx.EVT_BUTTON, self.add_hwpwm_click)
 
         # main sizer
@@ -31,7 +34,8 @@ class ctrl_pnl(wx.Panel):
         #main_sizer.AddStretchSpacer(1)
         main_sizer.Add(self.fill_table_btn, 0, wx.ALL|wx.EXPAND, 5)
         main_sizer.AddStretchSpacer(1)
-        main_sizer.Add(self.add_relay_btn, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.Add(self.lamp_confirm_btn, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.Add(self.add_relay_btn, 0, wx.ALL | wx.EXPAND, 5)
         main_sizer.AddStretchSpacer(1)
         main_sizer.AddStretchSpacer(1)
         main_sizer.Add(self.add_hbridge_btn, 0, wx.ALL|wx.EXPAND, 5)
@@ -63,6 +67,15 @@ class ctrl_pnl(wx.Panel):
         add_button = relay_dialog(i_pnl.relay_ctrl_lst, i_pnl.relay_ctrl_lst.parent)
         add_button.ShowModal()
         self.fill_tables_click("e")
+
+    def lamp_confirm_click(self, e):
+        i_pnl = self.parent.dict_I_pnl['power_pnl']
+        # load config from file and fill tables so there are no conflicts
+        self.fill_tables_click("e")
+
+        # call dialog box
+        lc_dbox = lampcon_dialog(self, self.parent)
+        lc_dbox.ShowModal()
 
     def add_hbridge_click(self, e):
         i_pnl = self.parent.dict_I_pnl['power_pnl']
@@ -1209,3 +1222,58 @@ class hwpwm_dialog(wx.Dialog):
 
     def show_guide_click(self, e):
         self.parent.parent.parent.shared_data.show_help('hwpwm_help.png')
+
+class lampcon_dialog(wx.Dialog):
+    def __init__(self, parent, *args, **kw):
+        self.parent = parent
+        super(lampcon_dialog, self).__init__(*args, **kw)
+        self.InitUI()
+        self.SetSize((800, 700))
+        self.SetTitle("Lamp with Confirmation")
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+
+    def InitUI(self):
+        #i_pnl = self.parent.parent.parent.dict_I_pnl['power_pnl']
+        shared_data = self.parent.parent.shared_data
+
+        # panel
+        pnl = wx.Panel(self)
+
+        # Header
+        box_label = wx.StaticText(self,  label='Lamp with Confirmation Test')
+        box_label.SetFont(shared_data.title_font)
+        # Show guide button
+        show_guide_btn = wx.Button(self, label='Guide', size=(175, 30))
+        show_guide_btn.Bind(wx.EVT_BUTTON, self.show_guide_click)
+        header_sizer =  wx.BoxSizer(wx.HORIZONTAL)
+        header_sizer.Add(box_label, 0, wx.ALL, 5)
+        header_sizer.AddStretchSpacer(1)
+        header_sizer.Add(show_guide_btn, 0, wx.ALL, 5)
+
+
+        # buttons_
+        self.save_btn = wx.Button(self, label='Save', size=(175, 30))
+        self.save_btn.Bind(wx.EVT_BUTTON, self.save_click)
+        self.cancel_btn = wx.Button(self, label='Cancel', size=(175, 30))
+        self.cancel_btn.Bind(wx.EVT_BUTTON, self.OnClose)
+        buttons_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        buttons_sizer.AddStretchSpacer(1)
+        buttons_sizer.Add(self.save_btn, 0,  wx.ALL, 3)
+        buttons_sizer.AddStretchSpacer(1)
+        buttons_sizer.Add(self.cancel_btn, 0,  wx.ALL, 3)
+        buttons_sizer.AddStretchSpacer(1)
+
+        main_sizer =  wx.BoxSizer(wx.VERTICAL)
+        main_sizer.Add(header_sizer, 0, wx.ALL|wx.EXPAND, 5)
+        main_sizer.AddStretchSpacer(1)
+        main_sizer.Add(buttons_sizer, 0, wx.ALL|wx.EXPAND, 5)
+        self.SetSizer(main_sizer)
+
+    def show_guide_click(self, e):
+        self.parent.parent.parent.shared_data.show_help('lampcon_help.png')
+
+    def OnClose(self, e):
+        self.Destroy()
+
+    def save_click(self, e):
+        print("Save is not written yet.")
